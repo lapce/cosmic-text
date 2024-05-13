cecc669e255b8e3fc8e53d34c6bde5551cc567f5
Mikael Urankar <mikael@FreeBSD.org>
2024-05-13 13:20:44 +0200

    Add FreeBSD support.

--- src/font/fallback/mod.rs
+++ src/font/fallback/mod.rs
@@ -10,7 +10,7 @@ use crate::{Font, FontSystem};
 
 use self::platform::*;
 
-#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows",)))]
+#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows", target_os = "freebsd",)))]
 #[path = "other.rs"]
 mod platform;
 
@@ -18,7 +18,7 @@ mod platform;
 #[path = "macos.rs"]
 mod platform;
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "freebsd"))]
 #[path = "unix.rs"]
 mod platform;
 
